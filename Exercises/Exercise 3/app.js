b4a73a70c8571a6476e56d62c79042d753678d99
Andrew Blair <blair.d.andrew@gmail.com>
2020-03-08 19:05:46 +0000

    wee changes

--- Exercises/Exercise 3/app.js
+++ Exercises/Exercise 3/app.js
@@ -12,12 +12,14 @@ new Vue({
         // }
     },
     watch: {
-        value: function (value) {
+        // i had this as value: which means it would not reference the computed above and then reset the timer before it got to 37. changed it to result: and it only starts the timer when it gets to 37
+        result: function () {
             let ls = this
             setTimeout(function() {
                 ls.value = 0
             }, 3000);
         }
+
 
     },
     methods: {
