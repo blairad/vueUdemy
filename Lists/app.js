f53cea0fd445892b4ec209da0a3edc848532f8d9
Andrew Blair <blair.d.andrew@gmail.com>
2020-03-24 17:48:52 +0000

    set up excercise 5

---
+++ Lists/app.js
@@ -0,0 +1,10 @@
+new Vue ({
+    el: "#app",
+    data: {
+        ingredients: ['meat', 'fruit', 'biscuits'],
+        persons: [
+            {name: 'Max', age: 27, colour: 'red'},
+            {name: 'Power', age: 34, colour: 'blue'}
+        ]
+    }
+});
