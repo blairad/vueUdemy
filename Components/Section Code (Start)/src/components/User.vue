8194ae9fafaa871109579da4f0b2f839c424730b
Andrew Blair <blair.d.andrew@gmail.com>
2020-04-09 12:35:33 +0100

    components example tut

---
+++ Components/Section Code (Start)/src/components/User.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change My Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail v-bind:myName="name" v-on:nameWasRest="name = $event"></app-user-detail>
+                <!--(shortcut is @name...) v-on:nameWasReset is listening for the $emit function from UserDetail.vue it does that with the $event name is fromthe data function the updated name is being passed as data this.myName -->
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return {
+                name: 'Andrew'
+            };
+        },
+        methods: {
+            changeName() {
+                this.name = 'Anna'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ Components/Section Code (Start)/src/components/UserDetail.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+        props: {
+            myName: {
+                type: String,
+                required: true
+            }
+            
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+                // split("") split on each character
+            },
+            resetName() {
+                this.myName = 'Andrew';
+                this.$emit('nameWasRest', this.myName)
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
